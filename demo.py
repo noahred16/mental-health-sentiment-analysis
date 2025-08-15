09d7a12f39f449d9e453f813ad484e5a4e2de56d
Noah Smith <nosmith16@gmail.com>
2025-08-15 05:01:41 -0400

    clean up comments

--- demo.py
+++ demo.py
@@ -4,16 +4,7 @@ import methods.RoBERTa as roberta
 import utils
 import pandas as pd
 
-# For the demo, we'll probably want to save our trained models and just load them in here.
-
-# We should try to make it so that we all use the same evaluation metrics and format.
-# So our methods should make sure to accept the data as parameters for training and testing.
-# Because we'll want to make sure that we use the same 80/20 split.
-# Alternatively we could use a random seed for reproducibility to ensure consistent results.
-
-# We also hopefully can benefit from sharing the same data preprocessing functions.
-
-# We can print out the results of each method here.
+# demo script to see the results of the pre-trained models against some test cases
 
 
 def compare_model_predictions():
